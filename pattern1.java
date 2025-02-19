895a51a3cd5e9d82637fb21bd248fa0566703f83
theseekerrr <priyaseekerrr@gmail.com>
2025-02-19 09:29:39 +0530

    1. Rectangle pattern

---
+++ pattern1.java
@@ -0,0 +1,15 @@
+//Rectangle pattern
+import java.util.*;
+public class pattern1{
+    public static void main(String args[]){
+        Scanner sc=new Scanner(System.in);
+        System.out.println("enter a number:");
+        int n=sc.nextInt();
+        for(int row =1;row<=n;row++){
+            for(int col=1;col<=n;col++){
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+    }
+}

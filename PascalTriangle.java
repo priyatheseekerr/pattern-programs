108735ee4f3c256d81f5437259379022d55ead36
theseekerrr <priyaseekerrr@gmail.com>
2025-06-01 10:53:16 +0530

    Pascal Triangle

---
+++ PascalTriangle.java
@@ -0,0 +1,23 @@
+import java.util.Scanner;
+
+public class PascalTriangle {
+    public static void main(String args[]) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter the number: ");
+        int n = sc.nextInt();
+        for (int i = 0; i < n; i++) {
+
+            for (int j = 0; j < n - i; j++) {
+                System.out.print("  ");
+            }
+
+            int num = 1;
+            for (int k = 0; k <= i; k++) {
+                System.out.print(num + "   ");
+                num = num * (i - k) / (k + 1);
+            }
+            System.out.println();
+        }
+        sc.close();
+    }
+}

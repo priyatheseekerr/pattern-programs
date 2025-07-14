f34a5bfcdf48f35a89822b9bd5def13ed82669b1
theseekerrr <priyaseekerrr@gmail.com>
2025-07-14 15:32:01 +0530

    Inverted pyramid

---
+++ InvertedPyramid.java
@@ -0,0 +1,21 @@
+import java.util.Scanner;
+
+public class InvertedPyramid {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter the number: ");
+        int n = sc.nextInt();
+        for (int i = 0; i < n; i++) {
+            for (int k= 0; k < i; k++) {
+                System.out.print(" ");
+            }
+            for (int j = 0; j <(2*(n-i)-1); j++) {
+                System.out.print("*");
+            }
+            
+
+            System.out.println();
+        }
+        sc.close();
+    }
+}
